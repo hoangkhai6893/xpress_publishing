aaf9326a820c5a26e575c846b97143233b95a275
Dang Hoang Khai <hoangkhai6893@gmail.com>
2020-05-28 14:48:06 +0900

    done write code

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const bodyParser = require('body-parser');
-const cors =  require('cors');
-const errorHandler = require('errorhandler');
-const morgan = require('morgan');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
 const express = require('express');
 
-const app = express();
+const apiRouter = require('./api/api');
 
-const PORT = process.env.PORT || 4000 ;
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
+app.use('/api', apiRouter);
 
+app.use(errorhandler());
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT);
+});
+
+module.exports = app;
